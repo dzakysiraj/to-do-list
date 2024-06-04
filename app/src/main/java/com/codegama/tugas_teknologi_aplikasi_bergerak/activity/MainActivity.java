9a6eab90f1ab2d3fbb6a2bf73848ba98832bcc41
dzakysiraj <dzakysiraj17@gmail.com>
2024-06-04 14:25:41 +0800

    Ini adalah pesan commit pertama anda

---
+++ app/src/main/java/com/codegama/tugas_teknologi_aplikasi_bergerak/activity/MainActivity.java
@@ -0,0 +1,116 @@
+package com.codegama.tugas_teknologi_aplikasi_bergerak.activity;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.codegama.tugas_teknologi_aplikasi_bergerak.R;
+import com.codegama.tugas_teknologi_aplikasi_bergerak.adapter.TaskAdapter;
+import com.codegama.tugas_teknologi_aplikasi_bergerak.bottomSheetFragment.CreateTaskBottomSheetFragment;
+import com.codegama.tugas_teknologi_aplikasi_bergerak.bottomSheetFragment.ShowCalendarViewBottomSheet;
+import com.codegama.tugas_teknologi_aplikasi_bergerak.broadcastReceiver.AlarmBroadcastReceiver;
+import com.codegama.tugas_teknologi_aplikasi_bergerak.database.DatabaseClient;
+import com.codegama.tugas_teknologi_aplikasi_bergerak.model.Task;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends BaseActivity implements CreateTaskBottomSheetFragment.setRefreshListener {
+
+    @BindView(R.id.taskRecycler)
+    RecyclerView taskRecycler;
+    @BindView(R.id.addTask)
+    TextView addTask;
+    TaskAdapter taskAdapter;
+    List<Task> tasks = new ArrayList<>();
+    @BindView(R.id.noDataImage)
+    ImageView noDataImage;
+    @BindView(R.id.calendar)
+    ImageView calendar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setUpAdapter();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        ComponentName receiver = new ComponentName(this, AlarmBroadcastReceiver.class);
+        PackageManager pm = getPackageManager();
+        pm.setComponentEnabledSetting(receiver, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+        Glide.with(getApplicationContext()).load(R.drawable.todolist).into(noDataImage);
+
+        addTask.setOnClickListener(view -> {
+            CreateTaskBottomSheetFragment createTaskBottomSheetFragment = new CreateTaskBottomSheetFragment();
+            createTaskBottomSheetFragment.setTaskId(0, false, this, MainActivity.this);
+            createTaskBottomSheetFragment.show(getSupportFragmentManager(), createTaskBottomSheetFragment.getTag());
+
+        });
+
+        getSavedTasks();
+
+        calendar.setOnClickListener(view -> {
+            ShowCalendarViewBottomSheet showCalendarViewBottomSheet = new ShowCalendarViewBottomSheet();
+            showCalendarViewBottomSheet.show(getSupportFragmentManager(), showCalendarViewBottomSheet.getTag());
+        });
+    }
+
+    public void setUpAdapter() {
+        taskAdapter = new TaskAdapter(this, tasks, this);
+        taskRecycler.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        taskRecycler.setAdapter(taskAdapter);
+    }
+
+    private void getSavedTasks() {
+
+        class GetSavedTasks extends AsyncTask<Void, Void, List<Task>> {
+            @Override
+            protected List<Task> doInBackground(Void... voids) {
+                tasks = DatabaseClient
+                        .getInstance(getApplicationContext())
+                        .getAppDatabase()
+                        .dataBaseAction()
+                        .getAllTasksList();
+                return tasks;
+            }
+
+            @Override
+            protected void onPostExecute(List<Task> tasks) {
+                super.onPostExecute(tasks);
+                noDataImage.setVisibility(tasks.isEmpty() ? View.VISIBLE : View.GONE);
+                setUpAdapter();
+            }
+        }
+
+        GetSavedTasks savedTasks = new GetSavedTasks();
+        savedTasks.execute();
+    }
+
+    @Override
+    public void refresh() {
+        getSavedTasks();
+    }
+}
